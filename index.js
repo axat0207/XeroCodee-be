d85337fe14949dc271819356822119b585811254
axat0207 <axxatagrawal@gmail.com>
2023-12-10 20:58:18 +0530

    hosting

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const connectToMongo = require('./db');
 const express = require('express')
+const serverless = require('serverless-http')
 var cors = require('cors') 
 
 connectToMongo();
 const app = express()
+const router = express.Router();
 const port = process.env.PORT || 8888
 
 app.use(cors())
@@ -17,3 +19,5 @@ app.use('/api/auth', require('./routes/auth'))
 app.listen(port, () => {
   console.log(`XeroCodee backend listening at http://localhost:${port}`)
 })
+app.use('/.netlify/functions/api', router)
+module.export.handler = serverless(app)
